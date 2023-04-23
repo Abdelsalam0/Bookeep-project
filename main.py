03c32e52cfe59c9fa6eebbc0df6cef9ef17d34ef
Abdelsalam Ahmed <abdelsalam.a.e@hotmail.com>
2023-04-23 22:14:58 +0200

    environment variable

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///Books.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
 db = SQLAlchemy(app)
